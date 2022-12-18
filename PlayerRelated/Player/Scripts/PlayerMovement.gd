8bb1fc170063ca336014dc2b6343856daab83c3c
farb <48386470+farbest@users.noreply.github.com>
2022-12-18 23:32:12 +0100

    added player animations

--- PlayerRelated/Player/Scripts/PlayerMovement.gd
+++ PlayerRelated/Player/Scripts/PlayerMovement.gd
@@ -3,13 +3,15 @@ extends KinematicBody2D
 onready var dashParticles = $DashParticles
 onready var playerPosition = $PlayerPosition
 onready var knifeReloadTimer = $KnifeReload
+onready var sprite = $Sprite
+onready var animationTree = $AnimationTree
 const knifeScene = preload("res://PlayerRelated/knife/Knife.tscn")
 
 var baseDamage = 100
 
 export (int) var walkSpeed = 400
 export (int) var dashSpeed = 2000
-export (int) var life = 3
+export (int) var healthPoints = 3
 export (float) var attackSpeed = 1.0
 export (float) var damage = baseDamage
 
@@ -28,37 +30,55 @@ func init_dash():
 	canDash = true
 	isDashing = false
 
-func dash():
-	var dashDirection = direction
+func dash(dashDirection):
 	if dashDirection.x == 0 && dashDirection.y == 0:
 		dashDirection = Vector2(1,0)
 	velocity = dashDirection.normalized() * dashSpeed
 	
 
 func player_movement():
-	if isDashing:
-		dash()
+	
+	var movingX = false
+	var movingY = false
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+		movingX = true
+
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+		movingX = true
+
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+		movingY = true
+
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+		movingY = true
+
+	var newDirection = Vector2()
+
+	if movingX || movingY :
+		if(movingX):
+			newDirection.x = direction.x
+		if(movingY):
+			newDirection.y = direction.y
+			
+		if isDashing:
+			dash(newDirection)
+		else:
+			velocity = newDirection.normalized() * walkSpeed
+			animationTree.get('parameters/playback').travel('run')
 	else:
-		direction = Vector2()
-		if Input.is_action_pressed("ui_right"):
-			direction.x += 1
-
-		if Input.is_action_pressed("ui_left"):
-			direction.x -= 1
-
-		if Input.is_action_pressed("ui_down"):
-			direction.y += 1
-
-		if Input.is_action_pressed("ui_up"):
-			direction.y -= 1
-
-		velocity = direction.normalized() * walkSpeed
+		animationTree.get('parameters/playback').travel('idle')
 		
-		if Input.is_action_just_pressed("dash"):
-			init_dash()
+	if Input.is_action_just_pressed("dash"):
+		init_dash()
 		
-	velocity.x = lerp(velocity.x,0,0.1)
-	velocity.y = lerp(velocity.y,0,0.1)
+	sprite.flip_h = direction.x == -1
+		
+	velocity.x = lerp(velocity.x,0,0.3)
+	velocity.y = lerp(velocity.y,0,0.3)
 
 func player_shooting():
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and knifeReloadTimer.is_stopped():
--- PlayerRelated/knife/Knife.gd
+++ PlayerRelated/knife/Knife.gd
@@ -8,4 +8,3 @@ func _physics_process(_delta):
 	var angle = get_global_transform().get_rotation()
 	velocity = Vector2(cos(angle), sin(angle))
 	velocity = move_and_collide(velocity*speed)
-	

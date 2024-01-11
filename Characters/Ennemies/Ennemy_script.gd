8b64313147498a5bb505b9c3f6f7e2e98c9c5b9c
farb <48386470+farbest@users.noreply.github.com>
2024-01-11 23:02:08 +0100

    feat: implemented different effect of hats

--- Characters/Ennemies/Ennemy_script.gd
+++ Characters/Ennemies/Ennemy_script.gd
@@ -4,15 +4,12 @@ func _on_area_2d_area_entered(_area):
 	self.take_damage(1)
 	
 func take_damage(amount:int):
-	self.health_points -= 1
-	self.die_if_dead()
-	
-func is_dead()->bool:
-	return self.health_points <= 0
-
-func die_if_dead():
-	if self.is_dead():
+	self.health_points -= amount
+	if self.should_die():
 		self.die() 
+	
+func should_die()->bool:
+	return self.health_points <= 0
 		
 func die():
 	queue_free()
--- Items/Hat_power_ups/Hat_power_up.gd
+++ Items/Hat_power_ups/Hat_power_up.gd
@@ -1,19 +1,82 @@
 extends Node2D
 
-@export var hat:String
-var hat_sprite = Sprite2D.new()
+enum HAT_TYPE{
+	BARBARIAN, 
+	CLERIC, 
+	FEATHER,
+	KNIGHT, 
+	PURPLE, 
+	TRICORN,
+	WIZARD,
+	}
+	
+@onready var sprite = $Sprite2D
+@export var hat_type: HAT_TYPE
+var hat_data = Hat_data.new()
 
-func _init():
-	hat_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/hats.png")
-	hat_sprite.hframes = 8
-	hat_sprite.vframes = 15
-	hat_sprite.frame = 16
+func get_hat_data(hat_type:HAT_TYPE):
+	
+	var hat_data:Hat_data = Hat_data.new()
+	var effect = Hat_effect.new()
+	var local_sprite = Sprite2D.new()
+	
+	var local_name = ""
+	
+	match hat_type:
+		HAT_TYPE.BARBARIAN:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/barbarian.png")
+			local_name = "barbarian"
+		HAT_TYPE.CLERIC:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/cleric.png")
+			local_name = "cleric"
+		HAT_TYPE.FEATHER:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/feather.png")
+			local_name = "feather"
+		HAT_TYPE.KNIGHT:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/knight.png")
+			local_name = "knight"
+		HAT_TYPE.PURPLE:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/purple.png")
+			local_name = "purple"
+		HAT_TYPE.TRICORN:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/tricorn.png")
+			local_name = "tricorn"
+		HAT_TYPE.WIZARD:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/wizard.png")
+			local_name = "wizard"
+		_:
+			# Effect modification
+			local_sprite.texture = preload("res://Items/Hat_power_ups/Hat_sprites/wizard.png")
+			local_name = "wizard"
+	return self.create_hat_data(local_name, effect,local_sprite)
+	
+func create_hat_data(name,effect,sprite):
+	var hat_data = Hat_data.new()
+	hat_data.set_name(name)
+	hat_data.set_effect(effect)
+	hat_data.set_sprite(sprite)
+	
+	return hat_data
+
+func _ready():
+	self.hat_data = self.get_hat_data(self.hat_type)
+	
+	self.hat_data.sprite.hframes = 8
+	self.hat_data.sprite.vframes = 2
+	self.hat_data.sprite.frame = 1
+	
+	sprite = self.hat_data.sprite.duplicate()
 
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("player"):
-		var hat_data:Hat_data = Hat_data.new().set_sprite(hat_sprite)
-		var effect = Hat_effect.new()
-		effect.speed_multiplier_increase = 2.00
-		hat_data.set_effect(effect)
-		body.pickup_item(hat_data)
+		body.pickup_item(self.hat_data)
 		queue_free()
+		
+		
